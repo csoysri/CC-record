5ca04d4af99e5df612bfa4b6380eb611391d4400
csoysri <chanasoysri@gmail.com>
2026-08-28 20:56:02 +0700

    Update recorder.py

--- recorder.py
+++ recorder.py
@@ -11,7 +11,7 @@ from google import genai
 TARGET_URL = "https://cdn-fr1-eu.lncoperations.ee/hls/cnbc_live/index.m3u8" 
 
 # 🛠️ ตั้งเวลาทดสอบ: อัด 30 วินาที / ตัดท่อนละ 15 วินาที
-RECORD_DURATION = 5400  
+RECORD_DURATION = 10800  
 SEGMENT_DURATION = 420  
 
 # 🔑 ดึง Key จาก GitHub Secret อัตโนมัติ
